9d5dbb1981ac9ce28fc3fd0893a872fdfc32d383
Nikolay <Onedaydejavu124@mail.ru>
2022-12-10 02:15:15 +0700

    вычислить a в степени n. Рекурсия

---
+++ EX002/Program.cs
@@ -0,0 +1,10 @@
+﻿// вычислить a в степени n. Рекурсия
+
+int PowerRec (int a, int n)
+{
+    //return n == 0 ? 1 : PowerRec(a, n-1) * a;
+    if(n == 0) return 1;
+    else return PowerRec(a, n-1) * a;
+}
+
+System.Console.WriteLine(PowerRec(2,10));
