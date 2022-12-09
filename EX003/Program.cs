2a211477015b368415bb443f081f2199ffb42883
Nikolay <Onedaydejavu124@mail.ru>
2022-12-10 02:32:14 +0700

    В алфавите 4 буквы а,и,с,в. покажите все слова, состоящие из Т букв этого алфавита. Рекурсия

---
+++ EX003/Program.cs
@@ -0,0 +1,16 @@
+﻿// В алфавите 4 буквы "а","и","с","в". покажите все слова, состоящие из Т букв этого алфавита. Рекурсия
+int n = 1;
+void FindWorlds (string alphabet, char[] world, int length = 0)
+{
+    if (length == world.Length)
+    {
+        Console.WriteLine($"{n++} {new String(world)}"); return;
+    }
+    for (int i = 0; i < alphabet.Length; i++)
+    {
+        world[length] = alphabet[i];
+        FindWorlds(alphabet, world, length + 1);
+    }
+}
+
+FindWorlds("аисв",new char[5]);
