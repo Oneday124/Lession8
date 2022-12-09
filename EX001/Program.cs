7c28988579bc44201cebaa12243d19a1de0f83e8
Nikolay <Onedaydejavu124@mail.ru>
2022-12-10 02:03:27 +0700

    Собрать строку с числами от a до b, a<=b. Рекурсия

---
+++ EX001/Program.cs
@@ -0,0 +1,8 @@
+﻿// Собрать строку с числами от a до b, a<=b. Рекурсия
+string NumbersRec(int a, int b)
+{
+    if(a <= b) return $"{a} " + NumbersRec(a+1,b);
+    else return String.Empty;
+}
+
+System.Console.WriteLine(NumbersRec(1,10));
