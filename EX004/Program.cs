28c31746d9f56627098e92fbd32fbf7c6d2fd02b
Nikolay <Onedaydejavu124@mail.ru>
2022-12-10 02:58:51 +0700

    Обход директории. Работа с директориями. С ошибками

---
+++ EX004/Program.cs
@@ -0,0 +1,31 @@
+﻿// Обход директории. Работа с директориями. С ошибками
+
+void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalog = new DirectoryInfo(path);
+    DirectoryInfo[] catalog = catalog.GetDiretories();
+    for(int i = 0; i < catalog.Length; i++)
+    {
+        System.Console.WriteLine($"{indent} {catalog[i].Name}");
+        CatalogInfo(catalog[i].FullName, indent + " ");
+    }
+
+    FileInfo[] files = Catalog.GetFiles();
+
+    for (int i = 0; i < files.Length; i++)
+    {
+        System.Console.WriteLine($"{indent}{files[i].Name}");
+    }
+}
+
+string path = @"C:/Users/Саша/Desktop/Обучение";
+CatalogInfo(path);
+
+//DirectoryInfo di = new DirectoryInfo(path);
+//System.Console.WriteLine(di.CreationTime);
+//FileInfo[] fi = di.GetFiles();
+
+//for(int i = 0; i < fi.Length; i++)
+//{
+//    System.Console.WriteLine(fi[i].Name);
+//}
