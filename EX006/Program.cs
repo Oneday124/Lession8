75d8e7127ddd2fa001602210ef2a140771bf9618
Nikolay <Onedaydejavu124@mail.ru>
2022-12-10 12:43:06 +0700

    Дерево traversal

--- EX006/Program.cs
+++ EX006/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Дерево traversal
+
+string emp = String.Empty;
+string[] tree = { emp, "/", "*", "10", "-", "+", emp, emp, "4", "2", "1", "3" };
+
+void InOrderTraversal(int pos = 1)
+{
+    if(pos < tree.Length)
+    {
+        int left = 2*pos;
+        int right = 2*pos+1;
+        if (left < tree.Length && !String.IsNullOrEmpty(tree[left])) InOrderTraversal(left);
+        System.Console.WriteLine(tree[pos]);
+        if (right<tree.Length && !String.IsNullOrEmpty(tree[right])) InOrderTraversal(right);
+    }
+}
+InOrderTraversal();
